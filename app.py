283442e66c64671ce26b353bc2ea25c366b88d83
Janahan <janahan@uncork.ai>
2019-01-13 20:51:28 -0500

    fixed db import

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.secret_key = 'secretkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity) # creates /auth
 
 api.add_resource(Item, '/item/<string:name>')
